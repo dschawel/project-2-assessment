f136d145e362ff47a9531ce91d384af6e2e15ff8
dschawel <david.schawel@gmail.com>
2020-01-10 16:12:55 -0800

    project 2 assessment.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+let db = require('./models')
 
 const app = express();
 
@@ -10,8 +11,35 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then(widgets => {
+    res.render('index', { widgets })
+    })
+})
 
+
+app.post('/', (req, res) => {
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+    .then(widget => {
+        res.redirect('/')
+    })
+})
+
+app.delete('/:id', (req, res) => {
+    db.widget.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((widget) => {
+        res.redirect('/')
+    })
+})
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
-app.listen(3000);
+app.listen(process.env.port || 3000);
